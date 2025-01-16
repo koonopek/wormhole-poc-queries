d8c83f2193642564a118a7735db559a1ee6f6b9e
Michal Konopka <konopek68@gmail.com>
2025-01-16 15:30:55 +0100

    feat: it works

--- main.js
+++ main.js
@@ -1,22 +1,48 @@
 const { QueryRequest, PerChainQueryRequest, EthCallQueryRequest, QueryProxyMock } = require("@wormhole-foundation/wormhole-query-sdk");
 const axios = require("axios");
+const ethers = require("ethers");
 
-const API_KEY = process.env["API_KEY"];
-const QUERY_URL = "https://testnet.query.wormhole.com/v1/query";
+const API_KEY = process.env["WORMHOLE_API_KEY"];
+const QUERY_URL = "https://query.wormhole.com/v1/query";
 const ETH_RPC = 'https://ethereum.publicnode.com';
 
-async function main() {
+const multicallAbi = [
+    "function aggregate3((address target, bool allowFailure, bytes callData)[] calls) external payable returns (tuple(bool success, bytes returnData)[])"
+];
 
+function encodeMulticallData() {
+    const multicallInterface = new ethers.Interface(multicallAbi);
+
+
+    const calls = [
+        {
+            target: "0xcA11bde05977b3631167028862bE2a173976CA11",
+            allowFailure: true,
+            callData: "0x3408e470"
+        }
+    ];
+
+    const encodedData = multicallInterface.encodeFunctionData("aggregate3", [calls]);
+
+    return {
+        to: "0xcA11bde05977b3631167028862bE2a173976CA11",
+        data: encodedData
+    };
+}
+
+async function main() {
     const blockTag = await getLatestBlockNumber();
     console.log({ blockTag })
 
-    const callData = { to: "0x61fFE014bA17989E743c5F6cB21bF9697530B21e", data: "0xc6a5026a000000000000000000000000c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2000000000000000000000000f951e335afb289353dc249e82926178eac7ded7800000000000000000000000000000000000000000000000031362a46ffe220d500000000000000000000000000000000000000000000000000000000000001f40000000000000000000000000000000000000000000000000000000000000000" };
+    const callData = encodeMulticallData();
+
+    console.log({ callData })
 
     const request = new QueryRequest(
         0, // Nonce
         [
             new PerChainQueryRequest(
-                1, // Ethereum Wormhole Chain ID
+                2, // Ethereum Wormhole Chain ID
                 new EthCallQueryRequest(blockTag, [callData])
             ),
         ]
@@ -50,13 +76,12 @@ async function ethCall({ to, data }, blockTag) {
             id: 1,
             jsonrpc: '2.0',
         })
-    ).data?.result;
+    ).data;
 
     return response;
 }
 
 async function getLatestBlockNumber() {
-    const rpc = 'https://ethereum.publicnode.com';
     const latestBlock = (
         await axios.post(ETH_RPC, {
             method: 'eth_getBlockByNumber',
